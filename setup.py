7c1f8f68b3486c3d8611e931e55f6acc29c46520
Peter Killick <peter.killick@informaticslab.co.uk>
2019-11-20 10:26:41 +0000

    Entrypoints (#2)
    * WIP: add entry_points
    
    * Update maintainer details

--- setup.py
+++ setup.py
@@ -9,13 +9,19 @@ setup(
     name='intake_iris',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='iris plugins for Intake',
+    description='Iris plugins for Intake',
     url='https://github.com/informatics-lab/intake-iris',
-    maintainer='Jacob Tomlinson',
-    maintainer_email='jacob.tomlinson@informaticslab.co.uk',
+    maintainer='Peter Killick',
+    maintainer_email='peter.killick@informaticslab.co.uk',
     license='BSD',
     py_modules=['intake_iris'],
     packages=find_packages(),
+    entry_points={
+        'intake.drivers': [
+            'netcdf = intake_iris.netcdf:NetCDFSource',
+            'grib = intake_iris.grib:GRIBSource',
+        ]
+    },
     package_data={'': ['*.csv', '*.yml', '*.html']},
     include_package_data=True,
     install_requires=requires,
